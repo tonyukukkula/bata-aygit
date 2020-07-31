910f296e4f764d07a80b058a41e2af22a7fc76dd
tonyukukkula <44534126+tonyukukkula@users.noreply.github.com>
2020-07-31 21:39:46 +0300

    örnek form yapısı gösterildi

---
+++ src/Ornek-Form/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.b3 = new System.Windows.Forms.Button();
+            this.ta1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(321, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Değer:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(500, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 85);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "tara";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(500, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 102);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // ta
+            // 
+            this.ta.Location = new System.Drawing.Point(235, 186);
+            this.ta.Name = "ta";
+            this.ta.Size = new System.Drawing.Size(183, 20);
+            this.ta.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 66);
+            this.label3.MaximumSize = new System.Drawing.Size(100, 500);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(235, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(500, 287);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(138, 65);
+            this.b3.TabIndex = 8;
+            this.b3.Text = "button3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // ta1
+            // 
+            this.ta1.Location = new System.Drawing.Point(235, 299);
+            this.ta1.Name = "ta1";
+            this.ta1.Size = new System.Drawing.Size(183, 20);
+            this.ta1.TabIndex = 9;
+            this.ta1.TextChanged += new System.EventHandler(this.Ta1_TextChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ta1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ta);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox ta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.TextBox ta1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ src/Ornek-Form/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,113 @@
+﻿//mevzubahis arduino kodları için örnek windows form app
+//Yazar: Alperen Kula
+//Yazım Tarihi: 20.05.2019
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public static SerialPort mp;
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+            //
+            //ldr();
+            //Console.WriteLine(ldr());
+            //Console.ReadLine();
+            mp = new SerialPort();
+            mp.PortName = "COM3";
+            mp.BaudRate = 9600;
+        }
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            label2.Text = ldr();
+        }
+        static public string ldr()
+        {
+          //  int deger;
+          //  mp.WriteLine("ldr");
+            string b = mp.ReadTo(";");
+            //deger = Convert.ToInt32();
+            return b;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mp.Open();
+        }
+        private void tarat() {
+            //if (!mp.IsOpen) { mp.Open(); }
+            Thread.Sleep(400);
+            mp.WriteLine("t");
+            string b = mp.ReadTo(";");
+            label2.Text = b;
+            //mp.Dispose();
+           // mp.Close();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //if (!mp.IsOpen) { mp.Open(); }
+            Thread.Sleep(400);
+            mp.WriteLine("k");
+            //Convert.ToInt32(ta.Text);
+            Thread.Sleep(400);
+            mp.WriteLine(ta.Text);
+            Thread.Sleep(400);
+
+            //if(mp.ReadTo("#")== "Waiting for valid finger to enroll as ")
+            //mp.Close();
+            //Please type in the ID # (from 1 to 127) you want to save this finger as $
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //if (!mp.IsOpen) { mp.Open(); }
+            Thread.Sleep(400);
+            mp.WriteLine("s");
+            //Convert.ToInt32(ta.Text);
+            Thread.Sleep(400);
+            mp.WriteLine(ta1.Text);
+            //if(mp.ReadTo("#")== "Waiting for valid finger to enroll as ")
+           // mp.Dispose();
+           // mp.Close();
+            //Please type in the ID # (from 1 to 127) you want to save this finger as $
+        }
+
+        private void Ta1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            tarat();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //mp.Dispose();
+            //mp.Close();
+        }
+    }
+}
